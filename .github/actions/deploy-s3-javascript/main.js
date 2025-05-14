a956b70035048e2c029e773f0a84d11025a00d77
gitcty <gitcty@gmail.com>
2025-05-14 00:31:50 -0400

    uploading artifacts using aws cli

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,17 @@ const github= require('@actions/github');
 const exec= require('@actions/exec');
 
 function run() {
+    // 1) Get input values for actions
+    const s3bucket = core.getInput('s3bucket', {required: true});
+    const region = core.getInput('region', {required:true});
+    const distfolder = core.getInput('dist-folder', {required: true});
+
+    // 2) Upload files
+    const s3URI = `s3:\\${s3bucket}`;
+
+    exec.exec(`aws s3 sync ${distfolder} ${s3URI} --region ${region}`);
+
+
     core.notice('Hello from my custom js action')
 }
 
