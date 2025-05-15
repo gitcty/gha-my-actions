4c281f07cb1d4e8e120d150afec44605d6b26100
gitcty <gitcty@gmail.com>
2025-05-14 21:57:05 -0400

    changing website content

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h3>It is cool to deploy using custom docker actions!!</h3>
       </header>
       <MainContent />
     </>
