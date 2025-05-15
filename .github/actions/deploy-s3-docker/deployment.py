e177be18a7ef1591e7f3ee343d99e10280f43835
gitcty <gitcty@gmail.com>
2025-05-14 21:54:51 -0400

    troubleshooting docker

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -22,7 +22,8 @@ def run():
                 ExtraArgs={"ContentType": mimetypes.guess_type(file)[0]}
             )
 
-    website_url = f'http://{bucket}.s3-website-{bucket_region}.amazonaws.com'
+    website_url = f'http://{bucket}.s3-website.{bucket_region}.amazonaws.com'
+    #http://gitcty-gha-actions-training-bkt.s3-website.us-east-2.amazonaws.com/
     # The below code sets the 'website-url' output (the old ::set-output syntax isn't supported anymore - that's the only thing that changed though)
     with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
         print(f'website-url={website_url}', file=gh_output)
